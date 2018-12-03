6421cf5fe71c2f881d7a6fd369375e6b98b6ce93
smanihwr <smanihwr@gmail.com>
2018-12-03 17:04:56 -0600

    added logging

--- src/main/java/com/example/messageparser/parser/MessageParser.java
+++ src/main/java/com/example/messageparser/parser/MessageParser.java
@@ -4,6 +4,8 @@ package com.example.messageparser.parser;
 import com.example.messageparser.model.Message;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,18 +13,28 @@ import java.io.FileReader;
 import java.util.List;
 
 public class MessageParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageParser.class);
+
     public static void main(String[] args) throws Exception {
+
         ObjectMapper mapper = new ObjectMapper();
 
         List<Message> messageList = mapper.readValue(
                 new FileReader("src/main/resources/sampledata.json"), new TypeReference<List<Message>>() {
                 });
 
-        System.out.println("Messages found: " + messageList.size());
+        LOG.debug("Messages found: " + messageList.size());
 
         messageList.parallelStream()
                 .forEach(message -> {
-                            HttpEntity<Message> request = new HttpEntity<>(message);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    HttpEntity<Message> request = new HttpEntity<>(message);
 
                             RestTemplate restTemplate = new RestTemplate();
                             String url = "http://localhost:8080/message-intake/api/message";
